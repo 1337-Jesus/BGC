a81e3fe4efe6fa6494a1cbd05033201c304cd2b0
1337-Jesus <37150668+1337-Jesus@users.noreply.github.com>
2020-07-17 16:23:00 +0200

    Create Header.h
    add Header file

---
+++ Header.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+
+void congratulation();
+unsigned int setTryCount();
+unsigned int setRandomNumber(std::mt19937 seed);
+unsigned int userGuess();
+char playAgain();
+
+void numberGuessingGame();
+
+
+void wordGue
